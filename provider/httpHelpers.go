031788bed082f455bbb8257ff7d88a4297a4cefb
Julian Espinel <julianespinel@gmail.com>
2020-05-04 15:09:00 +0300

    Return default error message when could not convert response body to string

--- provider/httpHelpers.go
+++ provider/httpHelpers.go
@@ -54,7 +54,8 @@ func deleteRequest(id string) (*http.Request, error) {
 func getResponseBodyAsString(resp *http.Response) string {
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Error(err)
+		log.WithField("status code", resp.StatusCode).WithError(err).Error("getResponseBodyAsString")
+		return "Could not convert response body to string"
 	}
 	return string(bodyBytes)
 }
